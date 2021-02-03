d717da1ef81aa3c29461dd6b4cc8bfc75ee21e01
Cyber-ASTR0 <68913762+Cyber-ASTR0@users.noreply.github.com>
2021-02-03 18:03:39 +0300

    Update unShort.sh

--- URLTool/unShort.sh
+++ URLTool/unShort.sh
@@ -1,6 +1,5 @@
 echo ""
-echo "Expands a short url & displays the original url."
-read -p "Enter You URL: " input_variable
+read -p "Enter Your Short URL: " input_variable
 if long=$(curl -s -o /dev/null --head -w "%{url_effective}\n" -L "$input_variable"); then
 echo "Original url -> $long"
 else
